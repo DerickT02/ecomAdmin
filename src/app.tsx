1ba6340ea08e6a481d82c67fb1677a4d0e77be92
DerickT02 <derickt18@outlook.com>
2022-12-06 13:27:48 -0800

    Updated UI and Adding Products to firebase successful

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,41 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
+import {addProduct} from './databaseQueries/queries'
 import './app.css'
 
 export function App() {
   const [count, setCount] = useState(0)
+  const [productName, setProductName] = useState("")
+  const [productPrice, setProductPrice] = useState(0.00)
+
+  const handleProductNameChange = (event: any) => {
+    setProductName(event.target.value)
+  }
+
+  const handleProductPriceChange = (event: any) => {
+    setProductPrice(event.target.value)
+  }
+
+  
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
+     
+      <h1>Welcome To Admin</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
+        <div class = "input">
+          <input placeholder= "Product Name" onChange = {handleProductNameChange}></input>
+          <input type = "number" placeholder= "Product Price" onChange = {handleProductPriceChange}></input>
+        </div>
+        <div class = "submit">
+          <button onClick = {() => {addProduct(productName, productPrice)}}>
+            Add Product
+          </button>
+        </div>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+        
+       
     </>
   )
 }
--- src/databaseQueries/queries.ts
+++ src/databaseQueries/queries.ts
@@ -1,5 +1,17 @@
+import { app, db } from './config'
+import { addDoc, collection } from 'firebase/firestore'
 
 
-export function addProduct(){
-    return 1;
+export async function addProduct(productName: string, productPrice: number){
+    let productRef = collection(db, 'products')
+    let addProductDoc = await addDoc(productRef, {
+        productName: productName,
+        productPrice: productPrice
+    }).then((res) => {
+        console.log("Successfully Added Product")
+    }).catch((err) => {
+        console.log("Unsuccessfully Added Product")
+    })
+    
 }
+
