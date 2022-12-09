3f78a0c402ec10bf1bd6b1ed620771e0ae0493b0
DerickT02 <derickt18@outlook.com>
2022-12-08 17:45:28 -0800

    Error Test correctly tests for Firebase Error

--- src/tests/queries.test.ts
+++ src/tests/queries.test.ts
@@ -10,8 +10,7 @@ describe("adding a product to the products collection in firebase", () => {
     //test a faulty input. The test should pass because we are testing a faulty inout
     it("Throws error since a field is left empty", async () => {
         expect.assertions(1)
-        await addProduct(undefined, 299.99).catch(err => {
-            expect(err).toEqual("FirebaseError: Function addDoc() called with invalid data. Unsupported field value: undefined \(found in field productName in document products\/[a-zA-Z]{20}\)")
+        await expect(addProduct(undefined, 299.99)).rejects.toThrowError()
         })
-    })
 })
+
