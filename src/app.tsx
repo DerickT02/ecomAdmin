b9ce605e2669f57a3e92c5389efae615b4506d52
DerickT02 <derickt18@outlook.com>
2022-12-24 08:47:32 -0800

    Refactoed Navbar code

--- src/app.tsx
+++ src/app.tsx
@@ -8,6 +8,7 @@ import { onAuthStateChanged } from 'firebase/auth'
 import './app.css'
 import ProductsList from './components/ProductsList'
 import CreateProducts from './components/CreateProduct'
+import Navbar from './components/Navbar'
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -97,23 +98,7 @@ useEffect(() => {
     <>
       <div class="card">
         {/*Navbar.tsx*/}
-        <div class = "nav-bar">
-          {isLoggedIn ? <><button onClick = {logout}>Logout</button></> : <>
-          {navIsClicked ? <>
-          <h2>{userFormAction}</h2>
-          <input placeholder={"email"} onChange = {(e) => {setEmail((e.target as HTMLInputElement).value)}}></input><input placeholder={"password"} type = "password" onChange={(e) => {setPassword((e.target as HTMLInputElement).value)}}></input>
-          {userFormAction == "Login" ? <button onClick = {() => {signIn(email, password)}}>Log In</button> : <button onClick = {() => {signUp(email, password)}}>Sign Up</button>}
-          <button onClick = {() => {setNavIsClicked(!navIsClicked)}}>Cancel</button>
-          </> : 
-          <>
-          <button onClick={() => {setUserFormAction("Login"); setNavIsClicked(!navIsClicked)}}>Login</button> <button onClick={() => {setUserFormAction("Sign Up"); setNavIsClicked(!navIsClicked)}}>Sign Up</button>
-          </>
-          }
-          
-          </>
-          }
-    
-        </div>
+        <Navbar isLoggedIn = {isLoggedIn}/>
         {/**/}
         {isLoggedIn ? <>
         {/*<div class = "input-form"> 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import { logout, signIn, signUp } from "../databaseQueries/auth"
+import { useState } from 'preact/hooks'
+interface Props{
+    isLoggedIn: boolean
+
+}
+
+export default function Navbar(props: Props){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [navIsClicked, setNavIsClicked] = useState(false)
+    const [userFormAction, setUserFormAction] = useState("")
+
+    return(
+        <>
+            <div class = "nav-bar">
+          {props.isLoggedIn ? <><button onClick = {logout}>Logout</button></> : <>
+          {navIsClicked ? <>
+          <h2>{userFormAction}</h2>
+          <input placeholder={"email"} onChange = {(e) => {setEmail((e.target as HTMLInputElement).value)}}></input><input placeholder={"password"} type = "password" onChange={(e) => {setPassword((e.target as HTMLInputElement).value)}}></input>
+          {userFormAction == "Login" ? <button onClick = {() => {signIn(email, password)}}>Log In</button> : <button onClick = {() => {signUp(email, password)}}>Sign Up</button>}
+          <button onClick = {() => {setNavIsClicked(!navIsClicked)}}>Cancel</button>
+          </> : 
+          <>
+          <button onClick={() => {setUserFormAction("Login"); setNavIsClicked(!navIsClicked)}}>Login</button> <button onClick={() => {setUserFormAction("Sign Up"); setNavIsClicked(!navIsClicked)}}>Sign Up</button>
+          </>
+          }
+          
+          </>
+          }
+    
+        </div>
+        </>
+    )
+}
