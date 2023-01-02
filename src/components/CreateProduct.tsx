e44666129211b85d3c1c44d40f22ed8d133608bd
DerickT02 <derickt18@outlook.com>
2023-01-02 15:56:07 -0800

    Added Loading UI whenever user chooses an image to create product

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'preact/hooks'
 import { addProduct } from '../databaseQueries/queries';
 import { storage } from '../databaseQueries/config';
-import { uploadBytes,ref, getDownloadURL } from 'firebase/storage';
+import { uploadBytes,ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
 
 
 export default function CreateProducts(){
@@ -9,6 +9,8 @@ export default function CreateProducts(){
     const [currImage, setCurrImage] = useState<File>();
     const [productName, setProductName] = useState("")
     const [productPrice, setProductPrice] = useState(0.00)
+    const [loadingState, setLoadingState] = useState("")
+
     const handleProductNameChange = (event: any) => {
         setProductName(event.target.value)
       }
@@ -18,19 +20,34 @@ export default function CreateProducts(){
       }
 
       const handleImageSubmit = () => {
+        
         uploadImageToFirebase(currImage)
       }
 
       async function uploadImageToFirebase(file: any){
-   
+       setLoadingState("")
         let productsImagesStorage = ref(storage, `productImages/${file.name}`)
-        uploadBytes(productsImagesStorage, file).then((snapshot) => {
-            getDownloadURL(snapshot.ref).then((url) => {
-                
-                setImgUrl(url)
-                console.log(imgUrl)
-            })
+        let uploadTask = uploadBytesResumable(productsImagesStorage, file)
+        uploadTask.on("state_changed", (snapshot) => {
+          console.log("Progress", (snapshot.bytesTransferred / snapshot.totalBytes) * 100, "%")
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              setLoadingState("Loading")
+              break;
+            case 'success':
+             
+          }
+        }, (error) => {
+          setLoadingState("Could Not Load Image")
+        }, () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
             
+            setImgUrl(downloadURL)
+            setLoadingState("Done")
+          });
         })
         
     }
@@ -39,16 +56,20 @@ export default function CreateProducts(){
         const files = (e.target as HTMLInputElement).files
         if(files && files.length >= 0){
           setCurrImage(files[0])
+          
+        
         }
         console.log("Files " + JSON.stringify(files))
         console.log(currImage?.name)  
         
        }
 
-       useEffect(() => {
-        console.log("File in State " + currImage)
+       useEffect(() => { 
+      }, [imgUrl])
+
+      useEffect(() => {
         handleImageSubmit()
-      }, [currImage, imgUrl])
+      },[currImage])
 
     return (
         <>
@@ -58,6 +79,7 @@ export default function CreateProducts(){
          <input placeholder= "Product Name" onChange = {handleProductNameChange}></input>
          <input type = "number" placeholder= "Product Price" onChange = {handleProductPriceChange}></input>
          <input type = "file" onChange = {handleImageChange}/>
+         {loadingState}
          <div class = "submit">
            <button onClick = {() => {addProduct(productName, productPrice, imgUrl)}}>
              Add Product
